ae88068c69fadd3d66c25b779e45e7a3bc7bc80d
kbcarte <kbcarte@gmail.com>
2021-07-19 20:30:29 -0400

    develop :: disabled convert button to stop spamming, updated README with more info

--- yt-to-mp3.py
+++ yt-to-mp3.py
@@ -54,6 +54,7 @@ def download_mp3(opts, urls):
         print('!'*40)
 
     padd()
+    window['Convert'].update(disabled=False)
     print("------------ All Done =) ------------")
 
 
@@ -84,13 +85,13 @@ while True:
 
     # Convert button clicked
     if event == 'Convert':
+        window['Convert'].update(disabled=True)
         urls = []
         for line in  values['-INPUT-'].split('\n'):
             if line != '':
                 line = line.rstrip() # remove white space
                 line = line.replace(',', '') # If CSV, remove commas
-                # remove any url params
-                # like playlists and time stamps
+                # remove any url params like playlists and time stamps
                 line = line.split('&')[0]
                 urls.append(line)
 
